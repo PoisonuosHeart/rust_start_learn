a0497a8527d93c2d2225893830411f6c277fd5d1
Poisonous Heart <poisonousheart@yandex.ru>
2022-03-13 15:01:22 +0300

    Revert "определение простого числа"
    This reverts commit 5be4dabad971712cbdb64962d5d3448308c8b98b.

--- other/simple_number/target/rls/debug/deps/simple_number-129a071d7704368b.d
+++
@@ -1,5 +0,0 @@
-c:\Git\rust\rust_start_learn\other\simple_number\target\rls\debug\deps\simple_number-129a071d7704368b.rmeta: src\main.rs
-
-c:\Git\rust\rust_start_learn\other\simple_number\target\rls\debug\deps\simple_number-129a071d7704368b.d: src\main.rs
-
-src\main.rs:
