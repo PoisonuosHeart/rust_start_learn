5be4dabad971712cbdb64962d5d3448308c8b98b
Poisonous Heart <poisonousheart@yandex.ru>
2022-03-13 14:59:40 +0300

    определение простого числа

---
+++ other/simple_number/target/debug/deps/simple_number.d
@@ -0,0 +1,5 @@
+C:\Git\rust\rust_start_learn\other\simple_number\target\debug\deps\simple_number.exe: src\main.rs
+
+C:\Git\rust\rust_start_learn\other\simple_number\target\debug\deps\simple_number.d: src\main.rs
+
+src\main.rs:
